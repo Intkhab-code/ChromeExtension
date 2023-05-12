8d46598298bb3ab0231158edc4ea2cab5d31580a
Intkhab-code <intkhabalam004.ia@gmail.com>
2023-05-12 11:59:16 +0530

    add and multiply

---
+++ index.js
@@ -0,0 +1,41 @@
+//  craete a function to calculate sum of any 2 numbs 
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+let result = sum(8, 7);
+console.log(result)
+
+
+let array = [2,3,4,5,6,7]
+// arraySum(array);
+
+// function arraySum(arr){
+//     let arrSum=0;
+//     for(let i=0; i < arr.length; i++){
+//         arrSum += arr[i];
+//     }
+//     console.log(arrSum);
+// }
+
+
+// const arraySum = (arr) => {
+//     let arrSum=0;
+//         for(let i=0; i < arr.length; i++){
+//             arrSum += arr[i];
+//         }
+//         console.log(arrSum);
+// }
+
+// arraySum(array)
+
+// const sumOfTwoNum = (a, b) => a + b
+// console.log(sumOfTwoNum(8, 7))
+
+// const multiplyTwoNum= (num1, num2) =>{
+//     return num1 * num2
+// } 
+// console.log(multiplyTwoNum(8, 7))
+
+const multiplyTwoNum=(num1, num2) => num1*num2
+console.log(multiplyTwoNum(8, 10));
