ecbcbd963850228461afd8bac3fa26bf6d303679
Intkhab-code <intkhab004.ia@gmail.com>
2023-05-13 15:02:52 +0530

    learn conditional statement and variables

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //  craete a function to calculate sum of any 2 numbs 
 
-function sum(num1, num2) {
-    return num1 + num2;
-}
-let result = sum(8, 7);
-console.log(result)
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+// let result = sum(8, 7);
+// console.log(result)
 
 
-let array = [2,3,4,5,6,7]
+// let array = [2,3,4,5,6,7]
 // arraySum(array);
 
 // function arraySum(arr){
@@ -37,5 +37,70 @@ let array = [2,3,4,5,6,7]
 // } 
 // console.log(multiplyTwoNum(8, 7))
 
-const multiplyTwoNum=(num1, num2) => num1*num2
-console.log(multiplyTwoNum(8, 10));
+// const multiplyTwoNum=(num1, num2) => num1*num2
+// console.log(multiplyTwoNum(8, 10));
+// function (){
+//     console.log('hello');
+// }
+
+// IIFE
+
+// (function (){
+//     console.log('hello');
+// })()
+
+// (function (a, b){
+//      return z= a+b;
+// })(5,6)
+
+// console.log(z);
+
+// // anonymus function
+// let x= function(){
+//     console.log('hello');
+// }
+// x()
+// var b=6;
+// var b=9;
+// console.log(b);
+
+// let a=5;
+// a=6;
+// console.log(a);
+
+// const z=5;
+// z=6;
+// console.log(z);
+
+//Conditional Statement
+// let a=6;
+
+// if(a<10){
+//     console.log('a is less than 10');
+// }if(a==6){
+//     console.log('a is equal to 6');
+// }
+// else{
+//     console.log('a is greater than 10');
+// }
+
+let b=4;
+switch (b) {
+    case 0:
+        console.log("result o");
+        break;
+
+    case 1:
+        console.log("result 1");
+        break
+case 2:
+    console.log("result 2");
+    break;
+
+    case 3:
+        console.log("result 3");
+        break;
+        default:
+            console.log("no result");
+        break;
+}
